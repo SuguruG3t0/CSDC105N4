797d304ba9e9b139cf8855351e2fd3786520236c
SuguruG3t0 <aagawa@gbox.adnu.edu.ph>
2025-03-12 19:35:31 +0800

    added server and packages stuff

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-// this is a server
-// javascript stuff
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req,res) => {
+    console.log('Good Morning CSDC105')
+    res.status(500).send('Hindi ka nya mahal')
+    
+})
+
+
+app.listen(3000)
